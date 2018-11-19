1281d5088d3146dd7c312a867d4be8f091385d03
Jonathan Apodaca <jrapodaca@gmail.com>
2018-11-18 20:39:04 -0700

    Calculator works!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(nll)]
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum Token {
     Number(f64),
     Plus,
@@ -15,8 +15,8 @@ enum Token {
 #[derive(Debug)]
 enum Node<'a> {
     Number(f64),
-    Unary(&'a Token, &'a Node<'a>),
-    Binary(&'a Node<'a>, Token, &'a Node<'a>),
+    Unary(&'a Token, Box<Node<'a>>),
+    Binary(Box<Node<'a>>, Token, Box<Node<'a>>),
 }
 
 fn eat_digits_as_f64(chars: &mut std::iter::Peekable<std::str::Chars>) -> Option<f64> {
@@ -103,55 +103,105 @@ impl<'a> Pratt<'a> {
     fn bp(&self, t: &Token) -> usize {
         match t {
             Token::RParen => 0,
-            Token::Number(_) => 5,
             Token::Plus => 10,
             Token::Minus => 10,
             Token::Times => 20,
             Token::Divide => 20,
             Token::Exponent => 30,
             Token::LParen => 40,
+            Token::Number(_) => 100,
         }
     }
 
-    fn nud(&mut self, t: &'a Token, bp: usize) -> Result<Node<'a>, &'a str> {
+    fn nud(&mut self, t: &'a Token, bp: usize) -> Result<Node<'a>, String> {
         match t {
             Token::Number(n) => Ok(Node::Number(*n)),
             Token::Plus | Token::Minus => {
-                let right = self.expr(bp)?;
-                Ok(Node::Unary(t, &right))
+                let right = self.expr(0)?;
+                Ok(Node::Unary(t, Box::new(right)))
             }
-            Token::LParen => Err("TODO"),
-            _ => Err(format!("Unexpected token {:?}", t).as_str()),
+            Token::LParen => {
+                let right = self.expr(0)?;
+                match self.tokens.next() {
+                    Some(Token::RParen) => Ok(right),
+                    _ => Err("Expected ')'".to_owned()),
+                }
+            }
+            _ => Err(format!("Unexpected token in NUD context: {:?}", t).to_owned()),
         }
     }
 
-    fn led(&mut self, left: Node<'a>, op: &Token, bp: usize) -> Result<Node<'a>, &'a str> {
-        Err("TODO")
+    fn led(&mut self, left: Node<'a>, op: &Token, bp: usize) -> Result<Node<'a>, String> {
+        match op {
+            Token::Plus | Token::Minus | Token::Times | Token::Divide => {
+                let right = self.expr(bp)?;
+                Ok(Node::Binary(Box::new(left), op.clone(), Box::new(right)))
+            }
+            Token::Exponent => {
+                let right = self.expr(bp - 1)?;
+                Ok(Node::Binary(Box::new(left), op.clone(), Box::new(right)))
+            }
+            _ => Err(format!(
+                "Unexpected token in LED context: {:?} (left={:?})",
+                op, left
+            )
+            .to_owned()),
+        }
     }
 
-    fn expr(&mut self, rbp: usize) -> Result<Node<'a>, &'a str> {
+    fn next_is_eof(&mut self) -> bool {
+        let peeked = self.tokens.peek();
+        peeked.is_none()
+    }
+    fn expr(&mut self, rbp: usize) -> Result<Node<'a>, String> {
         let err = "Undexpected EOF";
         let mut t = self.tokens.next().ok_or(err)?;
+
         let mut left = self.nud(t, self.bp(t))?;
-        println!("left = {:?}", left);
-        while rbp < self.bp(t) {
+        if self.next_is_eof() {
+            return Ok(left);
+        }
+        t = self.tokens.peek().unwrap();
+        while !self.next_is_eof() && rbp < self.bp(t) {
             let op = self.tokens.next().ok_or(err)?;
-            left = self.led(left, &op, self.bp(op))?;
-            t = self.tokens.next().ok_or(err)?;
+            left = self.led(left, op, self.bp(&op))?;
+
+            if self.next_is_eof() {
+                break;
+            }
+            t = self.tokens.peek().unwrap();
         }
         Ok(left)
     }
 }
 
+fn eval(node: Node) -> f64 {
+    match node {
+        Node::Number(n) => n,
+        Node::Unary(op, node) => match op {
+            Token::Minus => -eval(*node),
+            Token::Plus => eval(*node),
+            _ => 0_f64,
+        },
+        Node::Binary(left, op, right) => match op {
+            Token::Plus => eval(*left) + eval(*right),
+            Token::Minus => eval(*left) - eval(*right),
+            Token::Times => eval(*left) * eval(*right),
+            Token::Divide => eval(*left) / eval(*right),
+            Token::Exponent => eval(*left).powf(eval(*right)),
+            _ => 0_f64,
+        },
+    }
+}
+
 fn main() {
-    let source = "12.34 + 2*3 - 1^2^3".to_owned();
+    let source = "1+2*3^2^1".to_owned();
     let tkns = lex(&source);
 
     let mut tkns_iter = tkns.iter().peekable();
     let mut parser = Pratt::new(&mut tkns_iter);
-    let node = parser.expr(0);
-    println!(
-        "Hello, world! source={}; tkns={:?}; node={:?}",
-        source, tkns, node
-    );
+    let parse_result = parser.expr(0);
+    if let Ok(node) = parse_result {
+        println!("result={:?}", eval(node));
+    }
 }
